76fd49c1c6f746c838d5d3fe30b7cafbb4a75882
siryle <83765914+siryle@users.noreply.github.com>
2024-11-12 15:34:31 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -1,5 +1,5 @@
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
 
 # 修改亚瑟LED为绿色
 # sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
@@ -22,7 +22,7 @@ function git_sparse_clone() {
 
 # Alist & AdGuardHome & 集客无线AC控制器 & Lucky & AriaNg
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
+git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go
 git_sparse_clone master https://github.com/immortalwrt/packages net/ariang 
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
