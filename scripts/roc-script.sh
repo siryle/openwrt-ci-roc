8348a85d697d0a2749d76f929ca8abfe2a1baff6
siryle <83765914+siryle@users.noreply.github.com>
2024-11-13 14:12:15 +0800

    Update roc-script.sh

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -5,10 +5,7 @@ sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_genera
 # sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
 
 # 移除要替换的包
-rm -rf feeds/packages/net/alist
-rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/ariang
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -21,11 +18,7 @@ function git_sparse_clone() {
 }
 
 # Alist & AdGuardHome & 集客无线AC控制器 & Lucky & AriaNg
-git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go
-git_sparse_clone master https://github.com/immortalwrt/packages net/ariang 
-git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
 git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/mihomo
 
