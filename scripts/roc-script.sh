5911a1b94ae822e81de39f7beb02a4065bc35628
Roc Lai <laipeng668@qq.com>
2024-11-18 15:49:37 +0800

    精简项目并调整脚本

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -1,5 +1,6 @@
-# 修改默认IP
+# 修改默认IP & 固件名称
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
 
 # 修改亚瑟运行LED为绿色
 # sed -i 's/&status_blue/\&status_green/g' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6000-re-ss-01.dts
@@ -11,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/ariang
+# rm -rf feeds/packages/net/ariang
 rm -rf package/emortal/luci-app-athena-led
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -27,7 +28,7 @@ function git_sparse_clone() {
 # Alist & AdGuardHome & 集客无线AC控制器 & Lucky & AriaNg & 雅典娜LED控制
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
-git_sparse_clone master https://github.com/immortalwrt/packages net/ariang 
+# git_sparse_clone master https://github.com/immortalwrt/packages net/ariang 
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
